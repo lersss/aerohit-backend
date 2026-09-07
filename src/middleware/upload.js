a54e4e3e9cd462be6e5e307ea2acf73ce1b4c90b
lersss <skycomposer@gmail.com>
2026-09-07 21:23:53 +0700

    Update upload.js

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '../uploads'));
+    cb(null, '/data/uploads');
   },
   filename: (req, file, cb) => {
     const unique = Date.now() + '-' + Math.round(Math.random() * 1E9);
